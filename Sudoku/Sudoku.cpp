8db20c0381135751b774eb77f890494e74072548
Emma <emma.a.mayoral@gmail.com>
2022-07-09 05:06:15 -0700

    Checkers & call by reference starting row values
    I still need a sort of recursion function to check all order again whenever it runs a new row. It is under progress within lines 37, and 123

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -17,63 +17,86 @@ private:
     int B = 0;
 
 
+
 public:
     void start() //should this be your constructor
     {
+        srand(time(NULL));
         cout << "    Let's begin your 9x9 grid!" << endl << endl << endl << endl;
+        horizontalAssignment(0); //first row should be fine bc if will be the first array we will have to compare
 
-        horizontalAssignment(B); //we use parameters so that the program could be rerun at a certain verical point the numbers repreat.
-        verticalChecker();
+        for (int countRow = 1; countRow < 4; countRow++)
+        {
+            cout << countRow << endl;
+            srand(time(NULL)); //placement of this MATTERS
+            horizontalAssignment(countRow); //countRow is the line that will be compared with all the previous ones
+            verticalChecker();
+
+            cout << "This was run" << endl;
+
+            while (checker == true)
+            {
+                verticalChecker();
+                countRow = countRow-1;
+            }
+
+
+        }
+
+        
         gridDisplay();
-
-
-
 
     }
 
-    void horizontalAssignment(int b)
+    void horizontalAssignment(int row) //at start, should be equal to zero
     {
         int index1 = 0; //horizontal
 
         //cout << "~~~~~~~START OF ROUND~~~~~~~~" << endl; DEBUGGER
-        srand(time(NULL));
+        
+        int b = 0;
 
-
-
-        while (b < 4) //row - y axis
-        {
-
-            int a = 0;
-            while (a < 4) //column - x axis
-            {
-                arrayNumbers[b][a] = 0;
-                a++;
-            };
+        //while (b < 1) //row - y axis. b would give its starting y/row in which repition should be prevented
+        //{
 
             int horizontal[4] = { 1,2,3,4 };
 
+            int a = 0;
+            
+            /*  INCLUDE THIS IF YOU ARE THINKING TO CHECK ALL PREVIOUS COLUMN SLOTS. not necessary at the moment
+            while (a < 4) //column - x axis
+            {
+                horizontal[a] = 0;
+                a++;
+            };
+            */
+
+            /*------START OF ROW HERE---- - */
             a = 0;
             while (a < 4) //column - x axis
             {
-
                 index1 = rand() % 4;
                 bool indicator1 = false;
 
-                while (horizontal[index1] == 0 && indicator1 == false)
+                while (horizontal[index1] == 0 )
                 {
                     index1 = rand() % 4;
                 }
 
-                if (horizontal[index1] != 0 && indicator1 == false)
+                if (horizontal[index1] != 0 )
                 {
-                    arrayNumbers[b][a] = horizontal[index1];
-                    indicator1 = true;
+                    arrayNumbers[row][a] = horizontal[index1];
+                 
+                   cout << "Current slot value " << horizontal[index1] << endl;
                     horizontal[index1] = 0;
+                    
                     a++;
                 }
+                
             }
-            b++;
-        }
+            
+            //b++;
+       // }
 
         //cout << "~~~~~~~END OF ROUND~~~~~~~~" << endl; DEBUGGER
     }
@@ -82,49 +105,31 @@ public:
 
     void verticalChecker()
     {
-
+        checker = false;
         for (int column = 0; column < 4; column++)
         {
             for (int first = 0; first < 4; first++)
             {
                 for (int second = first + 1; second < 4; second++)
                 {
-                    while (arrayNumbers[first][column] == arrayNumbers[second][column])
+                    if (first!= second && arrayNumbers[first][column] == arrayNumbers[second][column])
                     {
                         cout << "column: " << column << endl; //DEBUGGER
                         cout << "first: " << first << endl; //DEBUGGER
                         cout << "second: " << second << endl << endl; //DEBUGGER
-
-                        B = second;
-                        horizontalAssignment(second); //row is returned
-                        verticalChecker2();
+                        gridDisplay();
+                        horizontalAssignment(second); //start from the x, end at that x on the why y axis.
+                        gridDisplay();
+                        checker = false;
+                       
+                       
                     }
                 }
             }
         }
+        
     }
 
-    void verticalChecker2()
-    {
-
-        for (int column = 0; column < 4; column++)
-        {
-            for (int first = 0; first < 4; first++)
-            {
-                for (int second = first + 1; second < 4; second++)
-                {
-                    while (arrayNumbers[first][column] == arrayNumbers[second][column])
-                    {
-                        B = second;
-                        horizontalAssignment(second); //row is returned
-
-                    }
-                }
-
-
-            }
-        }
-    }
 
 
     void gridDisplay()
@@ -157,8 +162,6 @@ public:
 };
 
 
-//Changes have been done
-
 
 int main() {
     cout << "           :) Herro! <3" << endl;
@@ -167,7 +170,7 @@ int main() {
     round.start();
 
 
-
+    system("pause");
     //include timer
 
     return 0;
