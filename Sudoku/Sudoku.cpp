d2c000f2b9570a57352eaa0278189a6443be448b
Emma <emma.a.mayoral@gmail.com>
2022-07-11 13:03:49 -0700

    User input of Blank Slots are now being displayed.
    I have added a few more functions that function with the user input of desired amount of blank spaces. The grid now displays the blank spaces.

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -1,132 +1,236 @@
-//Sudoku grid comes with a few spaces already filled in; the more spaces filled in, the easier the game � the more difficult Sudoku puzzles have very few spaces that are already filled in.
+﻿//Sudoku grid comes with a few spaces already filled in; the more spaces filled in, the easier the game – the more difficult Sudoku puzzles have very few spaces that are already filled in.
 //include keyboard
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <cstdlib> //randomizing library
-
+#include <cstdlib> //randomizing
 using namespace std;
 
 class gamePreparation
 {
-private:
-    int arrayNumbers[9][9];
-    int arrayBlanks[9][9];
-    int rowChecker = 0;
+    private:
+        //making the arrays strings will display blank spaces on the 9x9 grid
+        string arrayNumbers[9][9]; 
+        string arrayBlanks[9][9];
+        string displayedArray[9][9];
+        int rowChecker = 0;
 
-public:
-    void start() //should this be your constructor
-    {
-        //initializer();
-        srand(time(NULL));
-        cout << "    Let's begin your 9x9 grid!" << endl << endl << endl << endl;
-        horizontalAssignment(0); 
-        
-        for (int countRow = 1; countRow < 9; countRow++)
-        {  
-            horizontalAssignment(countRow); //countRow is the line that will be compared with all the previous lines
-            rowChecker = countRow+1; 
-            verticalChecker();         
-        }    
-        gridDisplay(); 
-    }
+    public:
 
-
-    void initializer() //very helpful for debugging. Maybe necessary to help initialize though. 
-    {
-        for (int b = 0; b < 9; b++) //row
+        void start() //should this be your constructor
         {
-            for (int a = 0; a < 9; a++) //column
+            numberInitializer();
+            blankInitializer();
+            srand(time(NULL));
+            horizontalAssignment(0); 
+        
+            for (int countRow = 1; countRow < 9; countRow++)
+            {  
+                horizontalAssignment(countRow); //countRow is the line that will be compared with all the previous lines
+                rowChecker = countRow+1; 
+                verticalChecker();         
+            }    
+
+            displayAssignment();
+            levelOfDifficulty();
+
+            cout << endl << "    Let's begin your 9x9 grid!" << endl << endl << endl << endl;
+            gridDisplay(); 
+        }
+
+        void levelOfDifficulty()
+        {
+            cout<<"[Choose the number of blank spots you would like to solve on your grid]"<<endl<<endl;
+            int decision = 0; //user input
+            int difficulty = 0; //pass by value
+
+            cout << " 1) 10 Blank Slots" << endl;
+            cout << " 2) 20 Blank Slots" << endl;
+            cout << " 3) 30 Blank Slots" << endl;
+            cout << " 4) 40 Blank Slots" << endl;
+            cout << " 5) 50 Blank Slots" << endl<<endl;
+            cout << " Enter your decision here: ";
+            cin >> decision;
+            cout << endl;
+
+            switch (decision)
             {
-                arrayNumbers[b][a]= 0;
+            case 1:
+                blankAssignment(10);
+                break;
+
+            case 2:
+                blankAssignment(20);
+                break;
+
+            case 3:
+                blankAssignment(30);
+                break;
+
+            case 4:
+                blankAssignment(40);
+                break;
+
+            case 5:
+                blankAssignment(50);
+                break;
+
+            default: 
+                cout << "[TRY AGAIN: Choose options 1-5]" << endl << endl;
+                levelOfDifficulty();
             }
         }
-    }
+        void displayAssignment()
+        {
 
-    void horizontalAssignment(int row) //at start, row is zero
-    {
-        int index1 = 0; //horizontal index
-        int a = 0; //column (x-axis)
-        int horizontal[9] = { 1,2,3,4,5,6,7,8,9 };
-        
-            /*------START OF ROW HERE-----*/
+            for (int b = 0; b < 9; b++) //row
+            {
+                for (int a = 0; a < 9; a++) //column
+                {
+                    displayedArray[b][a] = arrayNumbers[b][a];
+                }
+            }
+        }
 
-            while (a < 9) //column - x axis
+        void blankAssignment(int difficulty)
+        {
+            int index1 = 0;
+            int index2 = 0;
+            index1 = rand() % 9;
+            index2 = rand() % 9;
+            int count = 0;
+
+            while (count < difficulty)
             {
                 index1 = rand() % 9;
-                
+                index2 = rand() % 9;
 
-                while (horizontal[index1] == 0 )
+                while (displayedArray[index1][index2] == " ")
                 {
                     index1 = rand() % 9;
+                    index2 = rand() % 9;
                 }
 
-                if (horizontal[index1] != 0 )
+                if (displayedArray[index1][index2] != " ")
                 {
-                    arrayNumbers[row][a] = horizontal[index1];
-                    horizontal[index1] = 0;
-                   
-                    a++;
-                }           
+                    displayedArray[index1][index2] = " ";
+                    count++;
+                }
+
             }
-    }
 
-    void verticalChecker()
-    {
-     
-        for (int column = 0; column < 9; column++) 
+        }
+
+        void numberInitializer() //very helpful for debugging. Maybe necessary to help initialize though. 
         {
-            for (int firstRow = 0; firstRow < rowChecker-1; firstRow++)
+            for (int b = 0; b < 9; b++) //row
             {
-                for (int secondRow = firstRow + 1; secondRow < rowChecker; secondRow++)
+                for (int a = 0; a < 9; a++) //column
                 {
-                    if ( arrayNumbers[firstRow][column] == arrayNumbers[secondRow][column])
-                    {
-                        horizontalAssignment(secondRow);
+                    arrayNumbers[b][a]= "0";
+                }
+            }
+        }
 
-                        column = 0;
-                        firstRow = 0;
-                        secondRow = 1;
-                       
+        void blankInitializer() //very helpful for debugging. Maybe necessary to help initialize though. 
+        {
+            for (int b = 0; b < 9; b++) //row
+            {
+                for (int a = 0; a < 9; a++) //column
+                {
+                    arrayBlanks[b][a] = " ";
+                }
+            }
+        }
+
+        void horizontalAssignment(int row) //at start, row is zero
+        {
+            int index = 0; //horizontal index
+            int a = 0; //column (x-axis)
+            string horizontal[9] = { "1","2","3","4","5","6","7","8","9" };
+        
+                /*------START OF ROW HERE-----*/
+
+                while (a < 9) //column - x axis
+                {
+                    index = rand() % 9;
+                
+
+                    while (horizontal[index] == "0" )
+                    {
+                        index = rand() % 9;
+                    }
+
+                    if (horizontal[index] != "0" )
+                    {
+                        arrayNumbers[row][a] = horizontal[index];
+                        horizontal[index] = "0";
+                   
+                        a++;
+                    }           
+                }
+        }
+
+        void verticalChecker()
+        {
+            for (int column = 0; column < 9; column++) 
+            {
+                for (int firstRow = 0; firstRow < rowChecker-1; firstRow++)
+                {
+                    for (int secondRow = firstRow + 1; secondRow < rowChecker; secondRow++)
+                    {
+                        if ( arrayNumbers[firstRow][column] == arrayNumbers[secondRow][column])
+                        {
+                            horizontalAssignment(secondRow);
+                            column = 0;
+                            firstRow = 0;
+                            secondRow = 1;  
+                        }
                     }
                 }
-            }
+            }    
         }
-        
-    }
 
-    void gridDisplay()
-    {
-        int i = 0; //column (x-axis)
-        int j = 0; //row (y-axis)
-
-
-        while (j < 9) //row (y-axis)
+        void gridDisplay()
         {
-            cout << " ";
-            while (i < 8) //column (x-axis)
-            {
-                cout << arrayNumbers[j][i] << " | ";
-                i++;
-            }
-            cout << arrayNumbers[j][i];
-            cout << endl;
+            int i = 0; //column (x-axis)
+            int j = 0; //row (y-axis)
 
-            if (j < 8)
+
+            while (j < 9) //row (y-axis)
             {
-                cout << "-----------------------------------";
+                cout << " ";
+                while (i < 8) //column (x-axis)
+                {
+                    cout << displayedArray[j][i] << " | ";
+                    i++;
+                }
+                cout << displayedArray[j][i];
+                cout << endl;
+
+                if (j < 8)
+                {
+                    cout << "-----------------------------------";
+                }
+                cout << endl;
+                j++;
+                i = 0;
             }
-            cout << endl;
-            j++;
-            i = 0;
         }
-    }
+
+        void ending()
+        {
+            cout << setw(20) << "( ^o^)/\\(^_^ )" << endl;
+            cout << setw(20) << "You finished it in " << endl; // include stopwatch value here;
+            cout << setw(10) << "~~~Last 10 highest scores for this difficulty setting~~~" << endl;
+            cout << setw(15) << "[Would you like to play again?]" << endl;
+        }
 };
 
 
 
 int main() {
-    cout << setw(23) << ":) Herro! <3" << endl;
+    cout <<endl<< setw(40) << "\\(0_0)/  Hoi! " << endl;
 
     gamePreparation round;
     round.start();
